6165ad0bde096413380f677f7adf0170acf4aa75
kahyun0255 <kkh9946@naver.com>
2024-09-11 17:44:35 +0900

    feat : 장바구니 전체 삭제

--- coffee/src/main/java/programmers/coffee/cart/controller/CartController.java
+++ coffee/src/main/java/programmers/coffee/cart/controller/CartController.java
@@ -3,6 +3,7 @@ package programmers.coffee.cart.controller;
 import jakarta.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -62,4 +63,11 @@ public class CartController {
     public CartList cartList(HttpSession session){
         return (CartList) session.getAttribute("cartList");
     }
+
+    @DeleteMapping("/all")
+    public void deleteAll(HttpSession session){
+        session.removeAttribute("cartList");
+    }
+
+
 }
