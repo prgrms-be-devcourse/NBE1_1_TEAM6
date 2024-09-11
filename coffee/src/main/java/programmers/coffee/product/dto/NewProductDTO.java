75ca5e32bac2a675c0fbe1c0ecab2bb97caebb0a
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2024-09-11 22:24:36 +0900

    feat: 전체 상품 조회 페이징 기능 구현

--- coffee/src/main/java/programmers/coffee/product/dto/NewProductDTO.java
+++ coffee/src/main/java/programmers/coffee/product/dto/NewProductDTO.java
@@ -1,10 +1,14 @@
 package programmers.coffee.product.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import programmers.coffee.constant.Category;
 import programmers.coffee.constant.ProductStatus;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NewProductDTO {
 	private String productName;
 	private Category category;
