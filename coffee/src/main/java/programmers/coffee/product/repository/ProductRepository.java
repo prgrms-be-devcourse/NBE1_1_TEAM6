01279cd92645b6ceb7d4cdd399ed6ddb6b941d97
hgh1472 <hgh1472@naver.com>
2024-09-11 16:12:21 +0900

    refactor : Product PK 변경에 따른 리포지토리 코드 수정

--- coffee/src/main/java/programmers/coffee/product/repository/ProductRepository.java
+++ coffee/src/main/java/programmers/coffee/product/repository/ProductRepository.java
@@ -1,10 +1,8 @@
 package programmers.coffee.product.repository;
 
-import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import programmers.coffee.product.domain.Product;
 
-public interface ProductRepository extends JpaRepository<Product, UUID> {
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
